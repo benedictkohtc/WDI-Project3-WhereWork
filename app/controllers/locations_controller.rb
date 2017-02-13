97288148d6427684833c9827c656c5ad63d59458
Keith Chia <email@keithchia.com>
2017-02-13 18:21:16 +0800

    used figaro to make google picture api calls

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -8,8 +8,7 @@ class LocationsController < ApplicationController
 
   def show
     @last_updated_user = User.find(@location.last_updated_user) if @location.last_updated_user
-    @API_key = ENV['GOOGLE_PLACES_API_KEY']
-    # @API_key = 'AIzaSyCm9k1ny7YRfy9IVPQbrtaFQayu70QaScg'
+    @API_key = Figaro.env.GOOGLE_PLACES_API_KEY
   end
 
   def edit
