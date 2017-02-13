008b8b23182515900dd11079bf3da687186e2e1a
benedictkohtc <benedictkohtc@gmail.com>
2017-02-13 17:48:53 +0800

    added calc_distance function

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -25,4 +25,48 @@ class LocationsController < ApplicationController
     flash[:success] = 'Test SMS sent!'
     redirect_to action: 'secret'
   end
+
+  def get_distance
+    # receive user lat long in json
+    # return all location lat long in json within 400 m
+    # return true if result_distance < distance
+    # false
+  end
+
+  def calc_distance(lat1, lon1, lat2, lon2)
+    rad_per_deg = Math::PI / 180    # PI / 180
+    rkm = 6371                      # Earth radius in kilometers
+    rm = rkm * 1000                 # Radius in meters
+
+    dlat_rad = (lat1 - lat2) * rad_per_deg # Delta, converted to rad
+    dlon_rad = (lon1 - lon2) * rad_per_deg
+
+    lat1_rad = lat1 * rad_per_deg
+    lon1_rad = lon1 * rad_per_deg
+    lat2_rad = lat2 * rad_per_deg
+    lon2_rad = lon2 * rad_per_deg
+
+    a = Math.sin(dlat_rad / 2)**2 + Math.cos(lat1_rad) * Math.cos(lat2_rad) * Math.sin(dlon_rad / 2)**2
+    c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a))
+
+    rm * c # Delta in meters
+  end
 end
+
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
   authenticate :user do
     get 'locations/secret'
     get 'locations/twilio_test'
+    get 'locations/get_distance'
   end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
