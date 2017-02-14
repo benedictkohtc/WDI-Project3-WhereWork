065e51126b8079c7fd8c4ffe1162834edc516afa
benedictkohtc <benedictkohtc@gmail.com>
2017-02-14 13:35:50 +0800

    moved Twilio env vars to Figaro

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root 'locations#index'
   get 'locations/return_nearby_locations'
+  get 'locations/secret'
+  get 'locations/twilio_test'
 
   # custom devise resource
   devise_for :users
