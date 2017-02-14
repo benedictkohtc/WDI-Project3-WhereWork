6410c87f0f7d0f874c7d12a1bdca76973fab5433
Keith Chia <email@keithchia.com>
2017-02-14 11:07:22 +0800

    added list page passing in 10 of the first items from Location

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -3,7 +3,12 @@ require 'json'
 class LocationsController < ApplicationController
   before_action :find_location, only: [:show, :update, :edit]
   before_action :authenticate_user!, only: [:update, :edit]
+
   def index
+  end
+
+  def list
+    @locations = Location.limit(10)
   end
 
   def secret
